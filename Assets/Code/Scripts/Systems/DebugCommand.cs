842a1746e4e0d1f04bb85b111267f8787c719509
meme <mmeme0100@gmail.com>
2025-03-18 16:06:53 +0300

    added debugger

---
+++ Assets/Code/Scripts/Systems/DebugCommand.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+public class DebugCommand : DebugCommandBase
+{
+    private readonly Action _command;
+
+    public DebugCommand(string commandId, string commandDescription, string commandFormat, Action command) : base(commandId, commandDescription, commandFormat)
+    {
+        _command = command;
+    }
+
+    public void Invoke()
+    {
+        _command.Invoke();
+    }
+}
---
+++ Assets/Code/Scripts/Systems/DebugCommandBase.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class DebugCommandBase
+{
+    public string CommandId { get; }
+
+
+    public string CommandDescription { get; }
+    public string CommandFormat { get; }
+    protected DebugCommandBase(string commandId, string commandDescription, string commandFormat)
+    {
+        CommandId = commandId;
+        CommandDescription = commandDescription;
+        CommandFormat = commandFormat;
+    }
+}
