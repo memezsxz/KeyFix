a3ad8644a37b951451116006b242459229f2c3ff
FatimaAbdali <ffooffy35@gmail.com>
2025-04-28 19:09:00 +0300

    do some changes

---
+++ Assets/ImportedAssets/UnityTechnologies/3DBeginnerTutorial/Models/PressButton.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButton : MonoBehaviour
+{
+    public Transform redPart;
+    public Vector3 pressedOffset = new Vector3(0, -0.1f, 0);
+    private Vector3 originalPosition;
+    private Vector3 targetPosition;
+    public float moveSpeed = 5f;
+
+    private bool isPressed = false;
+
+    private void Start()
+    {
+        if (redPart != null)
+        {
+            originalPosition = redPart.localPosition;
+            targetPosition = originalPosition;
+        }
+    }
+
+    private void Update()
+    {
+        if (redPart != null)
+        {
+            redPart.localPosition = Vector3.Lerp(redPart.localPosition, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player") &&
+           collision.gameObject.CompareTag("button") &&
+           !isPressed)
+        {
+            targetPosition = originalPosition + pressedOffset;
+            isPressed = true;
+            Debug.Log("Button Pressed!");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player") && isPressed)
+        {
+            targetPosition = originalPosition;
+            isPressed = false;
+        }
+    }
+}
