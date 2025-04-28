0bf4980462cdda6ebd4c6c63b4d2788bb5ed758c
meme <mmeme0100@gmail.com>
2025-04-29 00:14:33 +0300

    corridor working

---
+++ Assets/Code/Scripts/Managers/SpaceManager.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceManager : MonoBehaviour
+{
+    [Header("Corridors in order")]
+    [SerializeField] private List<Corridor> corridors = new List<Corridor>();
+
+    private int currentCorridorIndex = -1;
+
+    private void Start()
+    {
+        DeactivateAllCorridors();
+    }
+
+    public void ActivateNextCorridor()
+    {
+        currentCorridorIndex++;
+
+        if (currentCorridorIndex >= corridors.Count)
+        {
+            Debug.Log("All corridors completed!");
+            return;
+        }
+
+        DeactivateAllCorridors();
+
+        Corridor nextCorridor = corridors[currentCorridorIndex];
+        if (nextCorridor != null)
+        {
+            nextCorridor.gameObject.SetActive(true);
+            nextCorridor.ActivateCorridor();
+        }
+    }
+
+    public void DeactivateAllCorridors()
+    {
+        foreach (var corridor in corridors)
+        {
+            if (corridor != null)
+            {
+                corridor.DeactivateCorridor();
+                corridor.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void ResetSpace()
+    {
+        currentCorridorIndex = -1;
+        DeactivateAllCorridors();
+    }
+}
