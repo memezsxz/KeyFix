573aa28ed58661d0c443e3b4c1fa8e150b21ecb4
meme <mmeme0100@gmail.com>
2025-04-30 00:49:29 +0300

    printer partical system working

---
+++ Assets/Code/Scripts/Managers/PaperPrinterParticleSystem.cs
@@ -0,0 +1,54 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class PaperPrinterParticleSystem : MonoBehaviour
+{
+    [Header("Spawn Settings")] [SerializeField]
+    private GameObject paperPrefab;
+
+    [SerializeField] private float spawnInterval = 1f;
+    private Transform modelTransform;
+
+    private Vector2 spawnAreaSize;
+    private float timer;
+
+    [Header("Paper Physics")] [SerializeField]
+    private float launchForce = 1f;
+
+    private void Start()
+    {
+        modelTransform = transform.parent.GetChild(0).GetComponent<Transform>();
+        if (modelTransform == null)
+        {
+            Debug.LogWarning("Child 'model' not found under Paper Printer Particle System!");
+        }
+
+        Vector3 localScale = transform.localScale;
+        spawnAreaSize = new Vector2(localScale.x, localScale.z);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval)
+        {
+            SpawnPaper();
+            timer = 0f;
+        }
+    }
+
+    private void SpawnPaper()
+    {
+        Vector3 localOffset = new Vector3(
+            Random.Range(-spawnAreaSize.x / 2f, spawnAreaSize.x / 2f),
+            0f,
+            Random.Range(-spawnAreaSize.y / 2f, spawnAreaSize.y / 2f)
+        );
+
+        Vector3 spawnPos = transform.TransformPoint(localOffset);
+
+        GameObject paper = Instantiate(paperPrefab, spawnPos, Quaternion.identity);
+        paper.transform.SetParent(modelTransform, worldPositionStays: true);
+    }
+}
