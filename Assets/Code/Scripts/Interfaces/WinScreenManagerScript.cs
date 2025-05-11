d2f529a2bcc4c09fda7e23b43f394773610b0dbd
FatimaAbdali <ffooffy35@gmail.com>
2025-05-11 16:28:14 +0300

    add final inteface

---
+++ Assets/Code/Scripts/Interfaces/WinScreenManagerScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreenManagerScript : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public AudioSource winAudio;
+    public float fadeDuration = 1f;
+    public float displayDuration = 2.5f;
+
+    private void Start()
+    {
+        canvasGroup.alpha = 0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void ShowWinScreen()
+    {
+        StartCoroutine(PlayWinSequence());
+    }
+
+    private IEnumerator PlayWinSequence()
+    {
+        // Fade In
+        yield return StartCoroutine(FadeCanvas(0f, 1f, fadeDuration));
+
+        // Play Sound
+        if (winAudio != null) winAudio.Play();
+
+        // Hold
+        yield return new WaitForSeconds(displayDuration);
+
+        // Fade Out
+        yield return StartCoroutine(FadeCanvas(1f, 0f, fadeDuration));
+    }
+
+    IEnumerator FadeCanvas(float from, float to, float duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(from, to, elapsed / duration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = to;
+        canvasGroup.interactable = (to > 0);
+        canvasGroup.blocksRaycasts = (to > 0);
+    }
+}
+
